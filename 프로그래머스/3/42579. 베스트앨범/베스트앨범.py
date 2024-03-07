8105d28a4de5035c77695a64ad427fe922f8e344
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-03-07 21:11:29 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -3,30 +3,18 @@ from collections import defaultdict
 def solution(genres, plays):
     answer = []
     
-    # 각 장르별 재생시간 저장하기 위해 defaultdict 사용
-    dic = defaultdict(int)
+    # 장르별 재생 횟수
+    genres_order = defaultdict(int)
+                               
+    # 장르별 각 고유번호 및 재생횟수
+    genres_plays = defaultdict(list)
     
-    # 장르, 재생시간, 고유번호 저장 배열
-    array = []
-    
-    for i in range(len(genres)):
-        # 각 장르별 재생시간 총합
-        dic[genres[i]] += plays[i]
-        # [장르, 재생시간, 고유번호 저장]
-        array.append([genres[i], plays[i], i])
-        
-    # 장르 재생시간을 기준으로 내림차순 정렬
-    sdic = sorted(dic.items(), key = lambda x: x[1], reverse=True)
-    
-    # [장르, 재생시간, 고유번호] 정렬
-    array = sorted(array, key = lambda x : (x[0], -x[1], x[2]))
-    
-    for i in range(len(sdic)):
-        # 최대 두 개씩만 앨범에 들어갈 수 있으니 카운트
-        count = 2
-        for j in range(len(array)):
-            if sdic[i][0] == array[j][0] and count != 0:
-                answer.append(array[j][2])
-                count -= 1
-                
+    for i, (genre, v) in enumerate(zip(genres, plays)):
+        genres_order[genre] += v
+        genres_plays[genre].append((i, v))
+                               
+    for genre, _ in sorted(genres_order.items(), key = lambda x: x[1], reverse = True):
+        for i, v in sorted(genres_plays[genre], key = lambda x: x[1], reverse = True)[:2]:
+            answer.append(i)
+            
     return answer
