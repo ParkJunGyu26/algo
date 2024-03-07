a6ca1a70d7726df5348f89bae134596173aa97a1
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-03-07 23:19:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.40 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -7,9 +7,10 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     unordered_map<string, int> map;
+    
     for (auto player : participant)
     {
-        if (map.end() == map.find(player))
+        if (map.find(player) == map.end())
             map.insert(make_pair(player, 1));
         else
             map[player]++;
@@ -19,11 +20,13 @@ string solution(vector<string> participant, vector<string> completion) {
         map[player]--;
     
     for (auto player : participant)
+    {
         if (map[player] > 0)
         {
             answer = player;
             break;
         }
+    }
     
     return answer;
 }
