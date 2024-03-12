daabc407312bdaf61b9c15184e37df5ecbf875c3
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-03-12 17:24:55 +0900

    [Silver II] Title: Three Dots, Time: 1188 ms, Memory: 255664 KB -BaekjoonHub

---
+++ 백준/Silver/13423. Three Dots/Three Dots.py
@@ -0,0 +1,24 @@
+import sys
+from collections import *
+
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+
+    n = int(input())
+    li = sorted(list(map(int, input().split())))
+    spotList = defaultdict(int)
+    for i in range(n):
+        spotList[li[i]] = 1
+
+    ans = 0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            first = li[i]
+            second = li[j]
+            third = li[j] + (li[j] - li[i])
+            if spotList[third] == 1:
+                ans += 1
+    
+    print(ans)
