2be8ff0220358e92ebb24a8810043afec15c07c1
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-03-14 17:48:55 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    if n <= 1:
+        ans = 1
+    else:
+        ans = factorial(n-1) * n
+
+    return ans
+
+print(factorial(int(input())))
