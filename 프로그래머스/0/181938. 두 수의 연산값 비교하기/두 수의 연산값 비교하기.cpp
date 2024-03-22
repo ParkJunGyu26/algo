5b7cb78070abbc4457ee4654d81f3549a8a77edd
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-03-22 16:31:53 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    answer = max(stoi(to_string(a) + to_string(b)), 2*a*b);
+    
+    return answer;
+}
