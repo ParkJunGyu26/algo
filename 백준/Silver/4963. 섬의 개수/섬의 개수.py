cef581660e771b19aed21d06fffe315594fca822
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-05-14 04:02:10 +0900

    [Silver II] Title: 섬의 개수, Time: 192 ms, Memory: 114884 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -3,28 +3,24 @@ from collections import deque
 input = sys.stdin.readline
 
 def bfs(x, y):
-    cnt = 0
-    q = deque()
-    q.append((x, y))
+    q = deque([(x, y)])
 
     while q:
         x, y = q.popleft()
 
-        for i in range(9):
+        for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-
             if -1 < nx < w and -1 < ny < h:
-                if not visited[ny][nx] and graph[ny][nx] == 1:
-                    q.append((nx, ny))
+                if not visited[ny][nx] and graph[ny][nx]:
                     visited[ny][nx] = True
-                    cnt = 1
+                    q.append((nx, ny))
     
-    return cnt
+    return 1
 
-dx = [0, 1, 1, 1, 0, -1, -1, -1, 0]
-dy = [1, 1, 0, -1, -1, -1, 0, 1, 0]
 
+dx = [0, 1, 1, 1, 0, -1, -1, -1]
+dy = [1, 1, 0, -1, -1, -1, 0, 1]
 while True:
     w, h = map(int, input().split())
     if w == 0 and h == 0:
@@ -32,10 +28,10 @@ while True:
 
     graph = [list(map(int, input().split())) for _ in range(h)]
     visited = [[False] * w for _ in range(h)]
-    cnt = 0
+    island = 0
     for i in range(h):
         for j in range(w):
-            if not visited[i][j] and graph[i][j] == 1:
-                cnt += bfs(j, i)
-    
-    print(cnt)
+            if not visited[i][j] and graph[i][j]:
+                visited[i][j] = True
+                island += bfs(j, i)
+    print(island)
