519d4d52f71b49bdcf76ac6d4125842d2cdbbc5d
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-05-28 13:41:28 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 252 ms, Memory: 115024 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,68 +1,51 @@
-# 이걸 어떻게 'dfs + 백트래킹' 으로 하지..?!
-# 음... 예제 3번이 총 개수가 60개가 나와야되는데 난 중복 포함돼서
-# 120번 나오네..
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 def dfs():
-    if len(stack) == len(cal):
-        all_sum = 0
-        for i in range(len(num)):
-            if i == 0:
-                all_sum = num[i]
+
+    if len(cal) == sachik_sum:
+        target = num[0]
+        for i in range(1, len(cal)+1):
+            if cal[i-1] == '+':
+                target += num[i]
+            elif cal[i-1] == '-':
+                target -= num[i]
+            elif cal[i-1] == '*':
+                target *= num[i]
             else:
-                if stack[i-1] == '+':
-                    all_sum += num[i]
-                elif stack[i-1] == '-':
-                    all_sum -= num[i]
-                elif stack[i-1] == '*':
-                    all_sum *= num[i]
+                if target < 0:
+                    target = target * -1
+                    target //= num[i]
+                    target = target * -1
                 else:
-                    if all_sum < 0:
-                        all_sum = all_sum * -1
-                        all_sum //= num[i]
-                        all_sum = all_sum * -1
-                    else:
-                        all_sum //= num[i]
-        
-        ans.append(all_sum)
+                    target //= num[i]
+        ans.append(target)
 
         return
-    
-    for i in range(len(cal)):
-        if not visited[i]:
-            visited[i] = True
-            stack.append(cal[i])
+
+    # 연산자가 4개니깐 4번 돌리기
+    for i in range(4):
+        if sachik[i]:
+            sachik[i] -= 1
+            cal.append(s[i])
+
             dfs()
-            visited[i] = False
-            stack.pop()
+
+            sachik[i] += 1
+            cal.pop()
 
 n = int(input())
 num = list(map(int, input().split()))
-tmp = list(map(int, input().split()))
+sachik = list(map(int, input().split()))
+sachik_sum = sum(sachik)
+
+s = {0:'+', 1:'-', 2:'*', 3:'/'}
 
 cal = []
-for i in range(4):
-    if i == 0:
-        for j in range(tmp[i]):
-            cal.append('+')
-    elif i == 1:
-        for j in range(tmp[i]):
-            cal.append('-')
-    elif i == 2:
-        for j in range(tmp[i]):
-            cal.append('*')
-    elif i == 3:
-        for j in range(tmp[i]):
-            cal.append('/')
-
-visited = [False] * len(cal)
-
-cnt = 0
-
-stack = []
 ans = []
+
 dfs()
+# print(len(ans))
+# print("ans : ", ans)
 print(max(ans))
 print(min(ans))
