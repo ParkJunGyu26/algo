0e37c2ff40dc0fe907deca43c6c692abe8e87595
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-06-27 18:14:39 +0900

    [Bronze III] Title: 크림빵, Time: 140 ms, Memory: 109240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -6,11 +6,11 @@ bread = list(map(int, input().split()))
 
 ans = 0
 for i in range(n):
-    pojang = 0
+    fail = 0
     for j in range(i*k, i*k+k):
-        if bread[j] == 1:
-            pojang += 1
-    if pojang >= p:
+        if bread[j] == 0:
+            fail += 1
+    if fail < p:
         ans += 1
 
 print(ans)
