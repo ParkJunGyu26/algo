ae7b2bd5b651c6cd010b712fa9ef898e73b9b66d
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-31 11:29:07 +0900

    [Silver III] Title: 수열, Time: 112 ms, Memory: 120956 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -3,10 +3,20 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 A = list(map(int, input().split()))
-prefix = []
+# 누적합
+# prefix = []
 
-prefix.append(sum(A[0:k]))
+# prefix.append(sum(A[0:k]))
+# for i in range(1, n-k+1):
+#     prefix.append(prefix[i-1] - A[i-1] + A[k+i-1])
+
+# print(max(prefix))
+
+# 슬라이딩 윈도우
+target = sum(A[:k])
+ans = target
 for i in range(1, n-k+1):
-    prefix.append(prefix[i-1] - A[i-1] + A[k+i-1])
+    target = target - A[i-1] + A[k+i-1]
+    ans = max(ans, target)
 
-print(max(prefix))
+print(ans)
