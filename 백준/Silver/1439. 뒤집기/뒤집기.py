9896262865139ff877edbd14eb6f2fbaf3e5ee4e
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-08-26 04:06:14 +0900

    [Silver V] Title: 뒤집기, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,18 @@
+S = list(input())
+
+one = 0
+zero = 0
+
+if S[0] == '0':
+    zero += 1
+else:
+    one += 1
+
+for i in range(1, len(S)):
+    if S[i] != S[i-1]:
+        if S[i] == '0':
+            zero += 1
+        else:
+            one += 1
+
+print(min(one, zero))
