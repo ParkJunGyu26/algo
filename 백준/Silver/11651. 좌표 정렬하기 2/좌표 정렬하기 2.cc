5e98d8278348fe488ec6103f76bc9b635c4cbf58
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-12-03 08:32:25 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -14,6 +14,10 @@ bool cmp(pair<int, int> a, pair<int, int> b) {
 }
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
