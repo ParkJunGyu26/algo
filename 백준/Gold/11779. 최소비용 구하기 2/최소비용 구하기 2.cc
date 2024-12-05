f64b359d247ce47e5ef77ac0805da695e59d3f39
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-12-06 08:40:49 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 32 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -2,61 +2,70 @@
 #include <vector>
 #include <queue>
 #include <climits>
+#include <algorithm>
 
 using namespace std;
 
-int n, m, a, b, c;
-pair<int, vector<int>> dist[1001];
+void dijkstra(int start, int end, vector<vector<pair<int, int>>>& graph) {
+    int n = graph.size() - 1;
+    vector<int> dist(n + 1, INT_MAX);
+    vector<int> parent(n + 1, 0);
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    
+    dist[start] = 0;
+    pq.push({0, start});
 
-void dijkstra(int node, vector<vector<pair<int, int>>> &graph, int target) {
-	priority_queue<pair<int, int>> pq;
-	for (int i = 1; i <= n; i++) dist[i].first = INT_MAX;
-	dist[node] = {0, {node}};
-	pq.push({0, node});
+    while (!pq.empty()) {
+        int now_dist = pq.top().first;
+        int now_node = pq.top().second;
+        pq.pop();
 
-	while(!pq.empty()) {
-		int now_dist = -pq.top().first;
-		int now_node = pq.top().second;
-		pq.pop();
+        // 현재 거리가 이미 기록된 거리보다 크면 스킵
+        if (now_dist > dist[now_node]) continue;
 
-		if (now_node == target) {
-			cout << now_dist << "\n" << dist[now_node].second.size() << "\n";
-			for (auto d : dist[now_node].second)
-				cout << d << " ";
-			return;
-		}
+        for (auto& edge : graph[now_node]) {
+            int next_node = edge.first;
+            int next_dist = edge.second;
+            
+            if (dist[next_node] > dist[now_node] + next_dist) {
+                dist[next_node] = dist[now_node] + next_dist;
+                parent[next_node] = now_node;
+                pq.push({dist[next_node], next_node});
+            }
+        }
+    }
 
-		for (int i = 0; i < graph[now_node].size(); i++) {
-			int next_dist = graph[now_node][i].second;
-			int next_node = graph[now_node][i].first;
-			vector<int> next_track = dist[now_node].second;
-			next_track.push_back(next_node);
-
-			if (dist[next_node].first > now_dist + next_dist) {
-				dist[next_node] = {now_dist + next_dist, next_track};
-				pq.push({-(now_dist + next_dist), next_node});
-			}
-		}
-	}
+    // 경로 추적 및 출력
+    vector<int> path;
+    for(int at = end; at != 0; at = parent[at]) {
+        path.push_back(at);
+    }
+    reverse(path.begin(), path.end());
+    
+    cout << dist[end] << "\n";  // 최소 비용
+    cout << path.size() << "\n";  // 도시 개수
+    for(int city : path) cout << city << " ";  // 경로
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int v1, v2;
+    int n, m, start, end;
+    cin >> n >> m;
+    
+    vector<vector<pair<int, int>>> graph(n + 1);
+    
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back({b, c});
+    }
 
-	cin >> n >> m;
-	vector<vector<pair<int, int>>> graph(n+1);
-	for (int i = 0; i < m; i++) {
-		cin >> a >> b >> c;
-		graph[a].push_back({b, c});
-	}
+    cin >> start >> end;
+    dijkstra(start, end, graph);
 
-	cin >> v1 >> v2;
-	dijkstra(v1, graph, v2);
-
-	// for (int i = 1; i <= n; i++)
-	// 	cout << "i : " << i << ", dist[i] : " << dist[i] << "\n";
+    return 0;
 }
