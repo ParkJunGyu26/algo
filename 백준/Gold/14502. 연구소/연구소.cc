e239da45ab62e9c39e100124ded9a8196604e8e4
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-12-08 20:54:30 +0900

    [Gold IV] Title: 연구소, Time: 48 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,83 +1,89 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int n, m;
+int n, m, answer = 0;
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
-int answer = 0;
+vector<pair<int, int>> empty_spaces, viruses;
 
-void bfs(queue<pair<int, int>> &q, vector<vector<int>> &v, vector<vector<int>> &graph) {
+void spread_virus(vector<vector<int>>& graph) {
+    vector<vector<int>> temp_graph = graph;
+    queue<pair<int, int>> q;
+    
+    for (auto& virus : viruses) {
+        q.push(virus);
+    }
 
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-			if (-1 < nx && nx < m && -1 < ny && ny < n && v[ny][nx] == 0 && graph[ny][nx] == 0) {
-				v[ny][nx] = 1;
-				graph[ny][nx] = 2;
-				q.push({nx, ny});
-			}
-		}
-	}
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && temp_graph[ny][nx] == 0) {
+                temp_graph[ny][nx] = 2;
+                q.push({nx, ny});
+            }
+        }
+    }
+
+    // 안전 영역 크기 계산
+    int safe_area = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (temp_graph[i][j] == 0) {
+                safe_area++;
+            }
+        }
+    }
+    answer = max(answer, safe_area);
 }
 
-void dfs(int num, int cnt, queue<pair<int, int>> &virus, vector<vector<int>> &graph, vector<vector<int>> &visited) {
-	if (cnt == 3) {
-		vector<vector<int>> v = visited;
-		vector<vector<int>> g = graph;
-		queue<pair<int, int>> q = virus;
-		bfs(q, v, g);
-		int count = 0;
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++) if (g[i][j] == 0) count++;
+void place_walls(int start, vector<vector<int>>& graph, int walls_placed) {
+    if (walls_placed == 3) {
+        spread_virus(graph);
+        return;
+    }
 
-		answer = max(answer, count);
-		return;
-	}
-
-	for (int i = num+1; i < n*m; i++) {
-		if (graph[i/m][i%m] == 0) {
-			graph[i/m][i%m] = 1;
-			dfs(i, cnt+1, virus, graph, visited);
-			graph[i/m][i%m] = 0;
-		}
-	}
+    for (int i = start; i < empty_spaces.size(); i++) {
+        int x = empty_spaces[i].first;
+        int y = empty_spaces[i].second;
+        
+        graph[x][y] = 1;
+        place_walls(i + 1, graph, walls_placed + 1);
+        graph[x][y] = 0;
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	cin >> n >> m;
-	vector<vector<int>> graph(n, vector<int>(m));
-	vector<vector<int>> visited(n, vector<int>(m));
-	queue<pair<int, int>> virus;
+    cin >> n >> m;
+    vector<vector<int>> graph(n, vector<int>(m));
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> graph[i][j];
-			if (graph[i][j] == 2) {
-				visited[i][j] = 1;
-				virus.push({j, i});
-			}
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> graph[i][j];
+            if (graph[i][j] == 0) {
+                empty_spaces.push_back({i, j});
+            }
+            if (graph[i][j] == 2) {
+                viruses.push_back({j, i});
+            }
+        }
+    }
 
-	for (int i = 0; i < m*n; i++) {
-		if (graph[i/m][i%m] == 0) {
-			graph[i/m][i%m] = 1;
-			dfs(i, 1, virus, graph, visited);
-			graph[i/m][i%m] = 0;
-		}
-	}
+    place_walls(0, graph, 0);
+    cout << answer;
 
-	cout << answer;
+    return 0;
 }
