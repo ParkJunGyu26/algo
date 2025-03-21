7fc36cc71eaae819144e50788396541bf4f51a38
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-03-22 07:30:15 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 72 ms, Memory: 8256 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,53 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <unordered_map>
+#include <stack>
 
 using namespace std;
 
-int n;
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> n;
-	vector<int> vec(n);
-	unordered_map<long long, long long> um;
-	for (int i = 0; i < n; i++) cin >> vec[i];
-
-	long long answer = 0;
-	vector<int> st;
-
-	int index = 0;
-
-	while (index < n) {
-
-		while (!st.empty() && st.back() < vec[index]) {
-			answer += um[st.back()];
-			um.erase(st.back());
-			st.pop_back();
-		}
-
-		if (!st.empty()) {
-			if (um.find(vec[index]) == um.end()) answer++;
-			else {
-				int i = st.size();
-				answer += um[st[i-1]];
-				if (st.size() >= 2 && st.back() == vec[index]) {
-					// answer += um[st[i-2]];
-					answer++;
-				}
-			}
-		}
-
-		if (um.find(vec[index]) == um.end()) {
-			um[vec[index]] = 1;
-			st.push_back(vec[index]);
-		} else um[vec[index]]++;
-		index++;
-	}
-
-	cout << answer;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> h(n);
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+    }
+    
+    stack<pair<int, int>> st; // {height, count}
+    long long answer = 0;
+    
+    for (int i = 0; i < n; i++) {
+        int cnt = 1;
+        while (!st.empty() && st.top().first <= h[i]) {
+            answer += st.top().second;
+            if (st.top().first == h[i]) {
+                cnt += st.top().second;
+            }
+            st.pop();
+        }
+        if (!st.empty()) {
+            answer++;
+        }
+        st.push({h[i], cnt});
+    }
+    
+    cout << answer;
+    return 0;
 }
