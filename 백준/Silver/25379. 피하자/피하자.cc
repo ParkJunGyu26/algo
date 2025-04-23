67a1911d137122ed69124047b76bb95057e88cb0
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-04-24 00:33:09 +0900

    [Silver IV] Title: 피하자, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/25379. 피하자/피하자.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int n;
+vector<int> vec;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	vec.resize(n);
+	for (int i = 0; i < n; i++) cin >> vec[i];
+
+	int odd = 0;
+	int even = 0;
+	int answer = 0;
+	for (int i = 0; i < n; i++) if (vec[i] % 2 == 0) even++; else odd++;
+
+	// 짝수 모두 왼쪽
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		if (i < even) {
+			if (vec[i] % 2 != 0) q.push(i);
+		} else {
+			if (vec[i] % 2 == 0) {
+				answer += (i - q.front());
+				q.pop();
+			}
+		}
+	}
+
+	// 짝수 모두 오른쪽
+	int cnt = 0;
+	queue<int> q2;
+	for (int i = 0; i < n; i++) {
+		if (i < odd) {
+			if (vec[i] % 2 == 0) q.push(i);
+		} else {
+			if (vec[i] % 2 != 0) {
+				cnt += (i - q.front());
+				q.pop();
+			}
+		}
+	}
+
+	cout << min(answer, cnt);
+}
