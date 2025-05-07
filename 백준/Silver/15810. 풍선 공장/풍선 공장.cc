b9326549470f0b6a770aeefa2ebff3570037b9e2
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-05-08 08:36:47 +0900

    [Silver II] Title: 풍선 공장, Time: 240 ms, Memory: 26724 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.cc
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.cc
@@ -1,21 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-long long n, m;
-vector<long long> vec;
-
-bool check(long long target) {
-	long long cnt = 0;
-	for (int i = 0; i < vec.size(); i++) {
-		cnt += (target / vec[i]);
-		if (cnt >= m) return true;
-	}
-
-	return false;
-}
+int cnt = 0;
+long long n, m, answer;
+priority_queue<pair<long long, long long>> pq;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -23,21 +13,21 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> n >> m;
-	vec.resize(n);
-	for (int i = 0; i < n; i++) cin >> vec[i];
-
-	long long left = 1;
-	long long right = *max_element(vec.begin(), vec.end()) * m +1;
-
-	while (left < right) {
-		long long mid = (left + right) / 2;
-
-		if (check(mid)) {
-			right = mid;
-		} else {
-			left = mid+1;
-		}
+	for (int i = 0; i < n; i++) {
+		long long tmp;
+		cin >> tmp;
+		pq.push({-tmp, -tmp});
 	}
 
-	cout << left;
+	while (!pq.empty() && cnt < m) {
+		long long now = -pq.top().first;
+		long long plus = -pq.top().second;
+		pq.pop();
+
+		cnt++;
+		pq.push({-(now+plus), -plus});
+		answer = now;
+	}
+
+	cout << answer;
 }
