cfa018e1afa98ecfb957679924b36f6ace0cf35f
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-05-21 13:08:57 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 264 ms, Memory: 30260 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -3,88 +3,50 @@ import java.io.*;
 
 public class Main {
     static int n, k;
-    static int MAX_SIZE;
-    static int[] visited;
-
-    static class Node {
-        int num;
-        ArrayList<Integer> list;
-
-        Node (int num, ArrayList<Integer> list) {
-            this.num = num;
-            this.list = list;
-        }
-    }
-
-    static boolean inRange(int number) {
-        return (0 <= number && number < MAX_SIZE);
-    }
-
-    static void bfs() throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        visited = new int[MAX_SIZE];
-        Arrays.fill(visited, Integer.MAX_VALUE);
-        Queue<Node> q = new LinkedList<>();
-        ArrayList<Integer> l = new ArrayList<>();
-
-        visited[n] = 0;
-        l.add(n);
-        q.offer(new Node(n, l));
-
-        while (!q.isEmpty()) {
-            Node current = q.poll();
-            int num = current.num;
-            ArrayList<Integer> list = current.list;
-
-            if (num == k) {
-                bw.write(String.valueOf(list.size()-1) + "\n");
-                for (int index : list) bw.write(String.valueOf(index) + " ");
-                bw.flush();
-                bw.close();
-                return;
-            }
-
-            int nNum = num * 2;
-            if (inRange(nNum) && visited[nNum] > visited[num] + 1) {
-                visited[nNum] = visited[num] + 1;
-                ArrayList<Integer> nList = new ArrayList<>(list);
-                nList.add(nNum);
-                q.offer(new Node(nNum, nList));
-            }
-
-            nNum = num + 1;
-
-            if (inRange(nNum) && visited[nNum] > visited[num] + 1) {
-                visited[nNum] = visited[num] + 1;
-                ArrayList<Integer> nList = new ArrayList<>(list);
-                nList.add(nNum);
-                q.offer(new Node(nNum, nList));
-            }
-
-            nNum = num - 1;
-            if (inRange(nNum) && visited[nNum] > visited[num] + 1) {
-                visited[nNum] = visited[num] + 1;
-                ArrayList<Integer> nList = new ArrayList<>(list);
-                nList.add(nNum);
-                q.offer(new Node(nNum, nList));
-            }
-
-        }
-    }
+    static int MAX_SIZE = 200_001;
+    static int[] visited = new int[MAX_SIZE];
+    static int[] parent = new int[MAX_SIZE];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        MAX_SIZE = Math.max(n, k) + 7;
-        
+
         if (n >= k) {
-            System.out.println(n - k);
-            for (int i = n; i >= k; i--) System.out.print(i + " ");
-        } else bfs();
+            bw.write((n - k) + "\n");
+            for (int i = n; i >= k; i--) bw.write(i + " ");
+            bw.flush();
+            bw.close();
+            br.close();
+            return;
+        }
+
+        Arrays.fill(visited, -1);
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(n);
+        visited[n] = 0;
+        parent[n] = -1;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (cur == k) break; // 올바른 종료 조건!
+            for (int next : new int[]{cur * 2, cur + 1, cur - 1}) {
+                if (0 <= next && next < MAX_SIZE && visited[next] == -1) {
+                    visited[next] = visited[cur] + 1;
+                    parent[next] = cur;
+                    q.offer(next);
+                }
+            }
+        }
+
+        bw.write(visited[k] + "\n");
+        Stack<Integer> stack = new Stack<>();
+        for (int i = k; i != -1; i = parent[i]) stack.push(i);
+        while (!stack.isEmpty()) bw.write(stack.pop() + " ");
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
