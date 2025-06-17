d306d68c95ec47243a04703b740ca7b8394a49fd
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-06-17 14:21:39 +0900

    [Gold III] Title: 소문난 칠공주, Time: 396 ms, Memory: 81308 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,101 +1,66 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static boolean[] visited = new boolean[25];
-    static char[][] graph = new char[5][5];
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static char[][] map = new char[5][5];
+    static boolean[] selected = new boolean[25];
     static int answer = 0;
-
-    static class Node {
-        int x, y;
-        Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static boolean check(ArrayList<Integer> hubo) {
-        boolean[][] isInGroup = new boolean[5][5];
-        int sCount = 0;
-
-        for (int num : hubo) {
-            int y = num / 5;
-            int x = num % 5;
-            isInGroup[y][x] = true;
-            if (graph[y][x] == 'S') {
-                sCount++;
-            }
-        }
-
-        if (sCount < 4) {
-            return false;
-        }
-
-        boolean[][] visitedBFS = new boolean[5][5];
-        Queue<Node> queue = new LinkedList<>();
-        int startY = hubo.get(0) / 5;
-        int startX = hubo.get(0) % 5;
-        queue.add(new Node(startX, startY));
-        visitedBFS[startY][startX] = true;
-        int count = 1;
-
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-
-            for (int d = 0; d < 4; d++) {
-                int nx = current.x + dx[d];
-                int ny = current.y + dy[d];
-
-                if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) {
-                    continue;
-                }
-
-                if (isInGroup[ny][nx] && !visitedBFS[ny][nx]) {
-                    visitedBFS[ny][nx] = true;
-                    count++;
-                    queue.add(new Node(nx, ny));
-                }
-            }
-        }
-
-        return count == 7;
-    }
-
-    static void dfs(int index, ArrayList<Integer> hubo) {
-        if (hubo.size() == 7) {
-            if (check(hubo)) {
-                answer++;
-            }
-            return;
-        }
-
-        for (int i = index; i < 25; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                hubo.add(i);
-                dfs(i + 1, hubo);
-                hubo.remove(hubo.size() - 1);
-                visited[i] = false;
-            }
-        }
-    }
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < 5; i++) {
             String line = br.readLine();
             for (int j = 0; j < 5; j++) {
-                graph[i][j] = line.charAt(j);
+                map[i][j] = line.charAt(j);
             }
         }
+        combination(0, 0, 0);
+        System.out.println(answer);
+    }
 
-        dfs(0, new ArrayList<>());
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        bw.close();
-        br.close();
+    // 7명을 뽑는 조합
+    static void combination(int idx, int cnt, int yCount) {
+        if (yCount >= 4) return; // Y가 4명 이상이면 가지치기
+        if (cnt == 7) {
+            if (isConnected()) answer++;
+            return;
+        }
+        for (int i = idx; i < 25; i++) {
+            selected[i] = true;
+            if (map[i / 5][i % 5] == 'Y') combination(i + 1, cnt + 1, yCount + 1);
+            else combination(i + 1, cnt + 1, yCount);
+            selected[i] = false;
+        }
+    }
+
+    // 선택된 7명이 연결되어 있는지 BFS로 체크
+    static boolean isConnected() {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[25];
+        for (int i = 0; i < 25; i++) {
+            if (selected[i]) {
+                q.offer(i);
+                visited[i] = true;
+                break;
+            }
+        }
+        int cnt = 1;
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            int x = now / 5, y = now % 5;
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d], ny = y + dy[d];
+                int next = nx * 5 + ny;
+                if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+                if (selected[next] && !visited[next]) {
+                    visited[next] = true;
+                    q.offer(next);
+                    cnt++;
+                }
+            }
+        }
+        return cnt == 7;
     }
 }
