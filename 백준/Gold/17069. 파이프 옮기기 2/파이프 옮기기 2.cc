8c37ba0367920321d5023d4e5b2e9543b418a1d9
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-06-23 19:11:13 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.cc
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.cc
@@ -4,50 +4,44 @@
 using namespace std;
 
 int n;
-vector<vector<int>> graph;
-vector<vector<vector<long long>>> dp;
-
-int dx[3] = {1, 1, 0};
-int dy[3] = {0, 1, 1};
-
-bool inRange(int x, int y) {
-	return (0 <= x && x < n && 0 <= y && y < n);
-}
-
-long long dfs(int x, int y, int dir) {
-	if (x == n - 1 && y == n - 1) return 1;
-
-	if (dp[y][x][dir] != -1) return dp[y][x][dir];
-
-	long long cnt = 0;
-	for (int i = 0; i < 3; i++) {
-		if ((dir == 0 && i == 2) || (dir == 2 && i == 0)) continue;
-
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		if (!inRange(nx, ny) || graph[ny][nx] == 1) continue;
-
-		if (i == 1) {
-			if (graph[y][x + 1] == 1 || graph[y + 1][x] == 1) continue;
-		}
-
-		cnt += dfs(nx, ny, i);
-	}
-	return dp[y][x][dir] = cnt;
-}
+vector<vector<int>> house;
+long long dp[32][32][3];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	cin >> n;
-	graph.resize(n, vector<int>(n));
-	dp.resize(n, vector<vector<long long>>(n, vector<long long>(3, -1)));
+    cin >> n;
+    house.assign(n, vector<int>(n));
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < n; x++)
+            cin >> house[y][x];
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) cin >> graph[i][j];
+    dp[0][1][0] = 1;
+    
+    for (int y = 0; y < n; y++) {
+        for (int x = 2; x < n; x++) {
+            if (house[y][x]) continue;
 
-	cout << dfs(1, 0, 0) << "\n";
+            dp[y][x][0] += dp[y][x - 1][0];
+
+            dp[y][x][0] += dp[y][x - 1][1];
+
+            if (y > 0)
+                dp[y][x][2] += dp[y - 1][x][2];
+
+            if (y > 0)
+                dp[y][x][2] += dp[y - 1][x][1];
+
+            if (y > 0 && !house[y - 1][x] && !house[y][x - 1]) {
+                dp[y][x][1] += dp[y - 1][x - 1][0];
+                dp[y][x][1] += dp[y - 1][x - 1][1];
+                dp[y][x][1] += dp[y - 1][x - 1][2];
+            }
+        }
+    }
+
+    long long result = dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2];
+    cout << result << '\n';
+    return 0;
 }
