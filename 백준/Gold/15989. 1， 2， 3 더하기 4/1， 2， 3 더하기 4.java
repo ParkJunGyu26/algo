9b9beb30721c2c1c5e4718c8b1fab21b1dce7d61
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2025-06-27 19:20:01 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 188 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -3,17 +3,15 @@ import java.io.*;
 
 public class Main {
     static int t, n;
-    static boolean[] num;
-    static long[] dp;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         t = Integer.parseInt(br.readLine());
-        dp = new long[10001];
-        num = new boolean[10001];
-        dp[1] = 1L;
-        dp[2] = 2L;
-        dp[3] = 3L;
+        dp = new int[10001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
         for (int i = 4; i < 10001; i++) {
             dp[i] = dp[i-1];
             if (i % 2 == 0) dp[i]++;
