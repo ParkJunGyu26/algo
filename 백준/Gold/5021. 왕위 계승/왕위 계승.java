3ef8584793a8235a5b988af9f1c853c106c55688
박준규 <junju404@naver.com>
2025-07-28 10:43:10 +0900

    [Gold IV] Title: 왕위 계승, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/5021. 왕위 계승/왕위 계승.java
+++ 백준/Gold/5021. 왕위 계승/왕위 계승.java
@@ -1,79 +1,60 @@
 import java.util.*;
 import java.io.*;
 
+// DFS + DP(바텀업)
 public class Main {
     static int n, m;
-    static HashMap<String, Long> answer;
-    static HashMap<String, Integer> parent;
-    static HashMap<String, ArrayList<String>> childTree;
+    static HashMap<String, Long> bloods;
+    static HashMap<String, String[]> parentArr;
 
-    static void topologySort(String root) {
-        Queue<String> q = new LinkedList<>();
-        q.offer(root);
+    static long dfs(String node) {
+        if (bloods.containsKey(node)) return bloods.get(node);
 
-        while (!q.isEmpty()) {
-            String node = q.poll();
+        if (parentArr.get(node) == null) return 0L;
 
-            for (String nNode : childTree.getOrDefault(node, new ArrayList<>())) {
-                parent.put(nNode, parent.get(nNode) - 1);
-                answer.put(nNode, answer.getOrDefault(nNode, 0L) + (answer.get(node) / 2));
-
-                if (parent.get(nNode) <= 0) {
-                    q.offer(nNode);
-                }
-            }
+        long blood = bloods.getOrDefault(node, 0L);
+        for (String nNode : parentArr.get(node)) {
+            blood += dfs(nNode) / 2;
         }
+
+        bloods.put(node, blood);
+        return blood;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        bloods = new HashMap<>();
+        parentArr = new HashMap<>();
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         String root = br.readLine();
-        parent = new HashMap<>();
-        childTree = new HashMap<>();
-        answer = new HashMap<>();
-
-        parent.put(root, 0);
         long MAX = 2L;
         for (int i = 0; i < n; i++) MAX *= 2;
-        answer.put(root, MAX);
 
+        bloods.put(root, MAX);
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            String[] tmp = new String[3];
-            for (int j = 0; j < 3; j++) tmp[j] = st.nextToken();
+            String[] info = new String[3];
+            for (int j = 0; j < 3; j++) info[j] = st.nextToken();
 
-            int cnt = 0;
-            cnt += (parent.get(tmp[1]) == null ? 0 : 1);
-            cnt += (parent.get(tmp[2]) == null ? 0 : 1);
-            parent.put(tmp[0], cnt);
-            
-            ArrayList<String> left = childTree.getOrDefault(tmp[1], new ArrayList<>());
-            ArrayList<String> right = childTree.getOrDefault(tmp[2], new ArrayList<>());
-            left.add(tmp[0]);
-            childTree.put(tmp[1], left);
-
-            right.add(tmp[0]);
-            childTree.put(tmp[2], right);
+            parentArr.put(info[0], new String[] {info[1], info[2]});
         }
 
-        topologySort(root);
-
-        String ans = "";
+        String answer = "";
         long max = 0L;
         for (int i = 0; i < m; i++) {
             String info = br.readLine();
-            long num = answer.getOrDefault(info, 0L);
+            long num = dfs(info);
 
-            if (max < num) {
+            if (num > max) {
                 max = num;
-                ans = info;
+                answer = info;
             }
         }
 
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
