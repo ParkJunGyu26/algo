b72a193e2236e5c1149bc6632ef00668b9658784
박준규 <junju404@naver.com>
2025-08-02 02:48:08 +0900

    [Gold II] Title: 불켜기, Time: 332 ms, Memory: 2824 KB -BaekjoonHub

---
+++ 백준/Gold/11967. 불켜기/불켜기.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int light = 1;
+int n, m;
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+vector<vector<int>> graph;
+vector<vector<vector<pair<int, int>>>> info;
+
+void bfs() {
+    vector<vector<bool>> visited(n, vector<bool>(n));
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    visited[0][0] = true;
+
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        bool isOn = false;
+
+        while (!info[y][x].empty()) {
+            int xx = info[y][x].back().first;
+            int yy = info[y][x].back().second;
+
+            graph[yy][xx] = 1;
+            info[y][x].pop_back();
+            isOn = true;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (!(-1 < nx && nx < n && -1 < ny && ny < n)) continue;
+
+            if (graph[ny][nx] == 0) continue;
+
+            if (isOn) {
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++) visited[j][k] = false;
+                q.push({nx, ny});
+                visited[ny][nx] = true;
+            }
+
+            if (!visited[ny][nx]) {
+                q.push({nx, ny});
+                visited[ny][nx] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    graph.resize(n, vector<int>(n));
+    info.resize(n, vector<vector<pair<int, int>>>(n));
+    graph[0][0] = 1;
+    
+    for (int i = 0; i < m; i++) {
+        int x, y, a, b;
+        cin >> x >> y >> a >> b;
+        info[y-1][x-1].push_back({a-1, b-1});
+    }
+
+    bfs();
+
+    int answer = 0;
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++) if (graph[i][j] == 1) answer++;
+    
+    cout << answer;
+}
