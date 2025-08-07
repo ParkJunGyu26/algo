ade56c374f04fbfe90461c190c6bd00b99090b13
박준규 <junju404@naver.com>
2025-08-07 11:34:27 +0900

    [Silver II] Title: 격자상의 경로, Time: 108 ms, Memory: 14308 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -2,52 +2,54 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m, k, answer = 0;
+    static boolean[][] visited;
     static int[] dx = {1, 0}, dy = {0, 1};
-    static int[][] graph;
+    static int[][] graph, path;
 
-    private static void bfs() {
+    private static int bfs(int[] start, int[] end) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0, 0});
+        q.offer(start);
+        for (int i = 0; i <= visited.length; i++) Arrays.fill(path[i], 0);
+
+        visited[start[1]][start[0]] = true;
+        path[start[1]+1][start[0]+1] = 1;
 
         while (!q.isEmpty()) {
-            int[] cur = q.poll(); // {x, y, k 만났는지}
-
-            if (cur[0] == (m-1) && cur[1] == (n-1)) {
-                if (cur[2] == 1) answer++;
-                continue;
-            }
+            int[] cur = q.poll();
 
             for (int i = 0; i < 2; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
 
-                if (!(-1 < nx && nx < m && -1 < ny && ny < n)) continue;
+                if (-1 < nx && nx <= end[0] && -1 < ny && ny <= end[1]) {
+                    if (!visited[ny][nx]) {
+                        visited[ny][nx] = true;
+                        q.offer(new int[] {nx, ny});
 
-                int status = cur[2];
-                if (status == 1) q.offer(new int[] {nx, ny, status});
-                else {
-                    status = (graph[ny][nx] == 1) ? 1 : 0;
-                    q.offer(new int[] {nx, ny, status});
+                        int nnx = nx + 1;
+                        int nny = ny + 1;
+                        path[nny][nnx] = path[nny-1][nnx] + path[nny][nnx-1];
+                    }
                 }
             }
         }
+
+        return path[end[1]+1][end[0]+1];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        k = Math.max(Integer.parseInt(st.nextToken()) - 1, 0);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Math.max(Integer.parseInt(st.nextToken()) - 1, 0);
         
         graph = new int[n][m];
-        if (k == 0) for (int i = 0; i < n; i++) Arrays.fill(graph[i], 1);
-        else graph[k / m][k % m] = 1;
+        visited = new boolean[n][m];
+        path = new int[n+1][m+1];
 
-        bfs();
-
-        System.out.println(answer);
+        if (k == 0) System.out.println(bfs(new int[] {0, 0}, new int[] {(m-1), (n-1)}));
+        else System.out.println(bfs(new int[] {0, 0}, new int[] {(k % m), (k / m)}) * bfs(new int[] {(k % m), (k / m)}, new int[] {(m-1), (n-1)}));
     }
 }
