9d0d03f0c6f3638db8746cb318f164fb221cd7f0
박준규 <junju404@naver.com>
2025-08-10 17:55:15 +0900

    [Gold IV] Title: 고냥이, Time: 140 ms, Memory: 15528 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, answer = 0;
+    static String S;
+    static int[] countArr;
+
+    // 투포인터 + 자료구조
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        S = br.readLine();
+
+        countArr = new int[27];
+        int left = 0;
+        int right = 0;
+
+        int cnt = 0; // 사용한 알파벳 개수
+        while (left <= right && right < S.length()) {
+            int rightChr = S.charAt(right) - 'a';
+
+            if (countArr[rightChr] == 0) {
+                while (cnt + 1 > n) {
+                    int leftChr = S.charAt(left) - 'a';
+                    countArr[leftChr]--;
+                    if (countArr[leftChr] == 0) cnt--;
+                    left++;
+                }
+                cnt++;
+            }
+            countArr[rightChr]++;
+            answer = Math.max(answer, (right - left + 1));
+            right++;
+        }
+        System.out.println(answer);
+    }
+}
