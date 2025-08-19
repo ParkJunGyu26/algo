3016097efc05c1fb1f51b07f537f12622f326ad9
박준규 <junju404@naver.com>
2025-08-19 12:00:52 +0900

    [Gold II] Title: 가운데를 말해요, Time: 420 ms, Memory: 33464 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,34 +1,63 @@
 import java.io.*;
 import java.util.*;
 
+// 가중치 음수라면 벨만-포드!!
+// 가중치 양수라면, 다익스트라 & 플로이드-워셜 & MST(최소신장트리)
+
 public class Main {
+    static int n;
+    static PriorityQueue<Integer> maxHeap, minHeap;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
         StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
+        maxHeap = new PriorityQueue<>((i1, i2) -> Integer.compare(i2, i1));
+        minHeap = new PriorityQueue<>((i1, i2) -> Integer.compare(i1, i2));
 
-            maxHeap.offer(num);
+        int num = Integer.parseInt(br.readLine());
+        minHeap.offer(num);
+        sb.append(num).append("\n");
 
-            if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
-                minHeap.offer(maxHeap.poll());
+        for (int i = 1; i < n; i++) {
+            num = Integer.parseInt(br.readLine());
+
+            if (minHeap.peek() < num) {
+                minHeap.offer(num);
+            } else {
+                maxHeap.offer(num);
             }
 
-            if (maxHeap.size() > minHeap.size() + 1) {
-                minHeap.offer(maxHeap.poll());
-            } else if (minHeap.size() > maxHeap.size()) {
-                maxHeap.offer(minHeap.poll());
+            if (minHeap.size() > maxHeap.size()) {
+                if (minHeap.size() > maxHeap.size() + 1 || maxHeap.peek() < minHeap.peek()) maxHeap.offer(minHeap.poll());
+            }
+
+            if (maxHeap.size() > minHeap.size()) {
+                if (maxHeap.size() > minHeap.size() + 1 || minHeap.peek() < maxHeap.peek()) minHeap.offer(maxHeap.poll());
             }
 
             sb.append(maxHeap.peek()).append("\n");
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
+
+/*
+
+3
+1
+3
+5
+-> 1 1 3
+
+5
+1
+5
+2
+4
+3
+-> 1 1 2 2 3
+
+ */
