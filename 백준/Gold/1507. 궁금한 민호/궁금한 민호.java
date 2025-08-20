ef11d3872ed39283178a98a81c6d7b909b076f5f
박준규 <junju404@naver.com>
2025-08-20 09:24:32 +0900

    [Gold II] Title: 궁금한 민호, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/1507. 궁금한 민호/궁금한 민호.java
+++ 백준/Gold/1507. 궁금한 민호/궁금한 민호.java
@@ -9,7 +9,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
+        // 주어진 최단 거리 2차원 배열 (원본)
         newGraph = new int[n][n];
+
+        // 초기 그래프(엣지)를 역추적하기 위한 2차원 배열
         graph = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,10 +21,12 @@ public class Main {
 
         int answer = 0;
         boolean check = true;
+
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (graph[i][j] == 0 || graph[i][k] == 0 || graph[k][j] == 0) continue;
+                    // 6 = 6 + 0 이러는 경우가 있기 때문에 스킵 (즉, 바로 이동할 수 있는 경우)
+                    if (graph[i][k] == 0 || graph[k][j] == 0) continue;
 
                     if (graph[i][j] == graph[i][k] + graph[k][j]) {
                         graph[i][j] = 0;
@@ -36,6 +41,7 @@ public class Main {
             }
         }
 
+        // 초기의 그래프를 통해 다시, 최단 거리 2차원 배열으로 플로이드 워셜로 만듦
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -49,6 +55,7 @@ public class Main {
             }
         }
 
+        // 초기 최단 거리 그래프와 초기 그래프를 통한 최단 거리 결과가 다른 경우는 불가능한 경우로 취급
         for (int i = 0 ; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (graph[i][j] != newGraph[i][j]) {
@@ -57,7 +64,6 @@ public class Main {
             }
         }
 
-        if (check) System.out.println(answer);
-        else System.out.println(-1);
+        System.out.println((check ? answer : -1));
     }
 }
